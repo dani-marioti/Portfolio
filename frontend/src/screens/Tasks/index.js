146b405b264911df1ff4f8c2c90471c45c3a9547
daniela.oliveira <daniela.oliveira@ngi.com.br>
2022-11-13 18:52:54 -0300

    Ideias - Analista

--- frontend/src/screens/Tasks/index.js
+++ frontend/src/screens/Tasks/index.js
@@ -1,16 +1,71 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { taskStyle } from './taskStyle'
+import apiIdeas from '../../services/ideias-api';
+import { FontAwesome } from 'react-native-vector-icons'
 
 class Tasks extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, backgroundColor: 'white' }}>
-                <View style={{ flex: 0, backgroundColor: 'white' }}>
-                    <Image source={require('../../assets/Img/logo.png')} />
-                </View>
-            </View>
-        );
+
+    constructor() {
+      super();
+      this.state = {
+        ideas: [],
+        selectedIdea: null,
+        visibleIdeaModal: false
+      };
     }
-}
+  
+    async componentDidMount() {
+      // inicia a tela
+      let res = await apiIdeas.getIdeas();
+      if (res.data && res.data.ideas) this.setState({ ideas: res.data.ideas });
+      console.log(this.state.ideas.length)
+    }
+  
+    goToIdea = () => {
+      this.props.navigation.navigate('Idea', {
+        onGoBack: async () => {
+          // inicia a tela
+          let res = await apiIdeas.getIdeas();
+          if (res.data && res.data.ideas) this.setState({ ideas: res.data.ideas });
+          console.log(this.state.ideas.length)
+        }
+      })
+    }
+    
+    renderItem = ({ item }) => (
+      <TouchableOpacity onPress={() => this.setState({ selectedIdea: item, visibleIdeaModal: true })} style={taskStyle.item} >
+        <View style={{ width: '20%', justifyContent: 'center', alignItems: 'center' }}>
+          <FontAwesome name="user" size={20} color="black" />
+          <Text>{item.name}</Text>
+        </View>
+  
+        <View style={{ width: '60%', height: '100%', padding: 10 }}>
+          <Text style={taskStyle.idea}>{item.title}</Text>
+          <Text numberOfLines={3} ellipsizeMode='tail'>{item.description}</Text>
+        </View>
+  
+        <View style={{ width: '20%', justifyContent: 'center', alignItems: 'center' }}>
+          <FontAwesome name="thumbs-up" size={20} color="black" />
+        </View>
+      </TouchableOpacity>
+    );
+  
+    render() {
+      return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+  
+          <Image style={{ marginTop: 30, alignSelf: 'center' }} source={require('../../assets/Img/logo.png')} />
+  
+          <FlatList
+            data={this.state.ideas}
+            keyExtractor={item => item.id}
+            renderItem={this.renderItem}
+          />
+ 
+        </View>
+      );
+    }
+  }
 
 export default Tasks;
---
+++ frontend/src/screens/Tasks/taskStyle.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native";
+
+const taskStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  button: {
+    backgroundColor: "#40e0d0",
+    justifyContent: 'center',
+    borderRadius: 5,
+    width: 100,
+    height: 30,
+    margin: 5
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 14,
+    alignSelf: "center",
+    color: "#FFFFFF"
+  },
+  button2: {
+    padding: 6,
+    borderRadius: 5,
+    fontWeight: "bold",
+    textDecorationLine: 'underline'
+  },
+  item: {
+    backgroundColor: '#ededed',
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 20,
+    borderRadius: 7,
+    flexDirection: 'row'
+  },
+  idea: {
+    backgroundColor: '#ededed',
+
+    fontWeight: "bold",
+  },
+    like:{
+        width:20,
+        height:20
+    }
+
+});
+export { taskStyle };
