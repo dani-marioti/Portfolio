3875c5e07e73a92a808bdc91843ed43bdcd21442
daniela.oliveira <daniela.oliveira@ngi.com.br>
2022-11-23 23:49:40 -0300

    Ordenar ideias

--- backend/src/controllers/IdeaController.ts
+++ backend/src/controllers/IdeaController.ts
@@ -7,7 +7,7 @@ export const findOne: RequestHandler = async (req, res) => {
   try {
     const found = await db.idea.findUnique({ where: { id } })
 
-    if(found) {
+    if (found) {
       return res.status(200).send({
         ...found
       })
@@ -21,54 +21,59 @@ export const findOne: RequestHandler = async (req, res) => {
 };
 
 export const findAll: RequestHandler = async (req, res) => {
-    res.status(200).send({
-      ideas: await db.idea.findMany()
-    })
+  res.status(200).send({
+    ideas: await db.idea.findMany({
+      orderBy: {
+        id: 'desc'
+      }
+    }
+    )
+  })
 };
 
 export const createIdea: RequestHandler = async (req, res) => {
   console.log(req.body)
-    try {
-      if(req.body) req.body.status = 'pending'; // no banco teria os tipos: pending, rejected, approved
-      const idea = await db.idea.create({ data: req.body })
+  try {
+    if (req.body) req.body.status = 'pending'; // no banco teria os tipos: pending, rejected, approved
+    const idea = await db.idea.create({ data: req.body })
 
-      res.status(201).send({
-        ...idea
-      });
-    } catch (error) {
-      console.error(error)
-      res.sendStatus(400);
-    }
+    res.status(201).send({
+      ...idea
+    });
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(400);
+  }
 };
 
 export const updateIdea: RequestHandler = async (req, res) => {
-    const id = Number(req.params.id);
-    try {
-      const idea = await db.idea.update({ data: req.body, where: { id } })
+  const id = Number(req.params.id);
+  try {
+    const idea = await db.idea.update({ data: req.body, where: { id } })
 
-      res.status(200).send({
-        ...idea
-      });
-    } catch (error) {
-      console.error(error)
-      res.sendStatus(400)
-    }
+    res.status(200).send({
+      ...idea
+    });
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(400)
+  }
 };
 
 export const deleteIdea: RequestHandler = async (req, res) => {
-    const id = Number(req.params.id);
-    if(!id) {
-      return res.sendStatus(400);
-    }
+  const id = Number(req.params.id);
+  if (!id) {
+    return res.sendStatus(400);
+  }
 
-    try {      
-      const deleted = await db.idea.delete({ where: { id } })
+  try {
+    const deleted = await db.idea.delete({ where: { id } })
 
-      if(deleted) {
-        return res.sendStatus(200)
-      }
-    } catch (error) {
-      console.error(error)
-      res.sendStatus(500);
+    if (deleted) {
+      return res.sendStatus(200)
     }
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500);
+  }
 };
