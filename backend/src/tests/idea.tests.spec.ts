d17fb9cc6ae545458445cf78fc17d69264c73614
daniela.oliveira <daniela.oliveira@ngi.com.br>
2022-12-06 21:08:49 -0300

    Testes

--- backend/src/tests/idea.tests.spec.ts
+++ backend/src/tests/idea.tests.spec.ts
@@ -1,8 +1,37 @@
 import { MockContext, Context, createMockContext } from './context';
 import { updateIdea, createIdea } from './functions-with-context';
 
+const fakePosts = [
+    {
+        id: 0,
+        title: 'Nova ideia',
+        description: 'Teste nova ideia',
+        name: 'Daniela',
+        email: 'dani@gmail.com',
+        status: 'aproved'
+    },
+    {
+        id: 1,
+        title: 'Olá',
+        description: 'descrição segunda ideia',
+        name: 'Joao',
+        email: 'joao@gmail.com',
+        status: 'aproved'
+    }
+]
+
 let mockCtx: MockContext
 let ctx: Context
+
+const prismaMock = {
+    post: {
+        create: jest.fn().mockReturnValue(fakePosts[0]),
+        findMany: jest.fn().mockResolvedValue(fakePosts),
+        findUnique: jest.fn().mockResolvedValue(fakePosts[0]),
+        update: jest.fn().mockResolvedValue(fakePosts[0]),
+        delete: jest.fn(), // O método delete não retorna nada
+    },
+};
 
 
 beforeEach(() => {
@@ -52,3 +81,15 @@ test('should update idea ', async () => {
     })
 })
 
+test(`findAll`, async () => {
+        prismaMock.post.create.mockResolvedValue(fakePosts)
+        expect(fakePosts).toEqual(fakePosts);
+
+    });
+
+
+test(`findOne`, async () => {
+        prismaMock.post.findUnique(fakePosts[0])
+        expect(fakePosts[0]).toEqual(fakePosts[0]);
+        expect(prismaMock.post.findUnique).toHaveBeenCalledWith(fakePosts[0]);
+    });
